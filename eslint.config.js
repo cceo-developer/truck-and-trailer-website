5ecb322501d76a63fcb773fcdf6122e723b3429a
justinornelas10 <jose.ornelas@cceo.com.mx>
2025-11-11 14:33:21 -0600

    ci-cd (workflows): :construction_worker: Add workflows and eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -4,21 +4,33 @@ import js from '@eslint/js'
 import pluginVue from 'eslint-plugin-vue'
 
 export default defineConfig([
-  {
-    name: 'app/files-to-lint',
-    files: ['**/*.{js,mjs,jsx,vue}'],
-  },
-
-  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
-
-  {
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-      },
+    {
+        name: 'app/files-to-lint',
+        files: ['**/*.{js,mjs,jsx,vue}'],
     },
-  },
 
-  js.configs.recommended,
-  ...pluginVue.configs['flat/essential'],
+    globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**', '**/.vite/**']),
+
+    js.configs.recommended,
+    ...pluginVue.configs['flat/essential'],
+
+
+    {
+        plugins: { vue: pluginVue },
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+            },
+        },
+        rules: {
+            'no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+
+            'vue/multi-word-component-names': 'off',
+            'vue/no-mutating-props': 'warn',
+            'vue/html-indent': ['warn', 4],
+            'vue/no-unused-vars': 'error',
+            'vue/block-order': ['warn', { order: ['template', 'script', 'style'] }],
+        }
+    },
+
 ])
